5333ded364e56281742eccd8766b763b0bd84a90
Jamie Keaveney <jamiekeaveney@MACBOOKPRO-C8AF.station>
2026-03-10 15:40:06 +0000

    Release v1.0.4

--- assets/js/core/page-transition-boilerplate.js
+++ assets/js/core/page-transition-boilerplate.js
@@ -74,52 +74,23 @@ function runPageOnceAnimation(next) {
   const tl = gsap.timeline();
 
   tl.call(() => {
-    resetPage(next);
+    resetPage(next)
   }, null, 0);
 
   return tl;
 }
 
 function runPageLeaveAnimation(current, next) {
-  const transitionWrap = document.querySelector("[data-transition-wrap]");
-  const transitionDark = transitionWrap.querySelector("[data-transition-dark]");
-
   const tl = gsap.timeline({
-    onComplete: () => {
-      current.remove(); 
-    }
-  })
-  
-  CustomEase.create("parallax", "0.7, 0.05, 0.13, 1");
+    onComplete: () => { current.remove() }
+  });
   
   if (reducedMotion) {
     // Immediate swap behavior if user prefers reduced motion
     return tl.set(current, { autoAlpha: 0 });
   }
-  
-  tl.set(transitionWrap, {
-    zIndex: 2
-  });
-  
-  tl.fromTo(transitionDark, {
-    autoAlpha: 0
-  },{
-    autoAlpha: 0.8,
-    duration: 1.2,
-    ease: "parallax"
-  }, 0);
-  
-  tl.fromTo(current,{
-    y: "0vh"
-  },{
-    y: "-25vh",
-    duration: 1.2,
-    ease: "parallax",
-  }, 0);
-  
-  tl.set(transitionDark, {
-    autoAlpha: 0,
-  });
+
+  tl.to(current, { autoAlpha: 0, duration: 0.4 });
 
   return tl;
 }
@@ -135,19 +106,12 @@ function runPageEnterAnimation(next){
     return new Promise(resolve => tl.call(resolve, null, "pageReady"));
   }
   
-  tl.add("startEnter", 0);
-  
-  tl.set(next, {
-    zIndex: 3
-  });
+  tl.add("startEnter", 0.6);
   
   tl.fromTo(next, {
-    y: "100vh"
-  }, {
-    y: "0vh",
-    duration: 1.2,
-    clearProps: "all",
-    ease: "parallax"
+    autoAlpha: 0,
+  },{
+    autoAlpha: 1,
   }, "startEnter");
 
   tl.add("pageReady");
